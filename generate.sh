a7e1d360a6a8d07bd69591daa51c67e6fac5ff04
Roy Bailey <roybaileybiz@gmail.com>
2020-01-03 09:45:09 +0000

    fix depenedency:tree capture file

--- generate.sh
+++ generate.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-# Additional source generation tooling
-mvn dependency:tree > mvndt.txt
+# Capture dependency:tree changes (strip out the noise and timestamps)
+mvn dependency:tree | awk '/Reactor Build Order/,/Reactor Summary for Neo4k/' > mvndt.txt
